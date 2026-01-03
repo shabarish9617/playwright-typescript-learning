1c64f8f3750dbbe8a57c68671fefb32514e0ab3b
Shabarish Karanji <shabarishkaranji@gmail.com>
2026-01-03 23:36:23 +0530

    Day2: Variables, datatypes, Arrays

---
+++ src/day2.ts
@@ -0,0 +1,55 @@
+let username:String="Shabarish";
+let age:number=25;
+let location:string="HYD";
+let ismetro: boolean=true;
+let skills : Array<number | String >=["25","Shabarish"];
+
+let user={
+   name:username,
+   ag:age,
+   loc:location,
+   met:ismetro
+}
+console.log(user);
+console.log(skills);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function newvar()
+// {
+//     let sum=100;
+//     console.log(sum);
+// }
+// newvar();
+// const name= "Shabarish";
+// console.log(name);
+
+//Datatypes -primitive and non-primitive data types
+/*number
+string
+any
+null
+undefined
+Union type
+boolean
+void*/
+
+// let data:number=20;
+// console.log(data);
+
+// let empname:string[]=[];
+// empname[0]="John";
+// empname[1]="King";
+// empname[2]="Major";
+// console.log(empname);
